8a48c5343c3834c7999057a98d5c6d1a17e6778e
Athene Allen <atheneallen93@gmail.com>
2025-09-23 21:19:29 +0100

    coda to undertale entry

--- SiteTools/ParticleGen/obj/Debug/net8.0/ParticleGen.AssemblyInfo.cs
+++ SiteTools/ParticleGen/obj/Debug/net8.0/ParticleGen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParticleGen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46ab3f11b87a0ed9af99e267b0aa412acf0bcc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b325a9c15740667e5eca386baa34a563064a92bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParticleGen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParticleGen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
